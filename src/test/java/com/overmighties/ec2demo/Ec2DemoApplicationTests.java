14170038161260ab8ba105d175167b8b10b43107
Sewery <seweryn.tasior@op.pl>
2024-09-25 13:58:28 +0200

    Second commit

---
+++ src/test/java/com/overmighties/ec2demo/Ec2DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.overmighties.ec2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class Ec2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        boolean bol=true;
+        assertTrue(bol);
+    }
+
+}
